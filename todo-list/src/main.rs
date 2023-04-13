07ed3d5d62d7aeefdd09192a72b945573d93eb3b
hardik badola <hardikb3732@gmail.com>
2023-04-13 18:43:22 +0530

    [ADDING] todo

--- todo-list/src/main.rs
+++ todo-list/src/main.rs
@@ -12,10 +12,19 @@ fn main() {
         false,
     );
 
+    let new_todo_2 = Todo::new(
+        2,
+        String::from("new todo 2"),
+        String::from("this is desc 2"),
+        true,
+    );
+
     let mut todo_list = Todos::new();
 
     todo_list.add(new_todo);
-    todo_list.delete(2);
+    todo_list.add(new_todo_2);
+
+    todo_list.delete(4);
 
     println!("{}", todo_list);
 }
