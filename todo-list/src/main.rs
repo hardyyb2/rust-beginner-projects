4bb82ed95782c4588f242c907abb6ed5d09879a0
hardik badola <hardikb3732@gmail.com>
2023-04-15 19:24:16 +0530

    [ADDED] show menu function

--- todo-list/src/main.rs
+++ todo-list/src/main.rs
@@ -1,7 +1,7 @@
 mod todo;
 mod todos;
 
-use std::io;
+use std::{io, process};
 use uuid::Uuid;
 
 use todo::Todo;
@@ -24,8 +24,49 @@ fn create_todo() -> Todo {
     new_todo
 }
 
+const MENU_OPTIONS: [&'static str; 4] = ["add todo", "delete todo", "edit todo", "exit"];
+
+fn show_menu(todo_list: &mut Todos) {
+    loop {
+        println!("Menu - ");
+        for (index, option) in MENU_OPTIONS.iter().enumerate() {
+            println!("{}. {}", index + 1, option);
+        }
+
+        let mut user_input = String::new();
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("failed to get option");
+
+        let selected_option = user_input.trim().parse();
+
+        match selected_option {
+            Ok(1) => {
+                let new_todo = create_todo();
+                todo_list.add(new_todo);
+                // println!("created new todo - {}", new_todo);
+            }
+            Ok(2) => {
+                println!("please enter the id for todo");
+
+                let mut delete_id = String::new();
+                io::stdin()
+                    .read_line(&mut delete_id)
+                    .expect("failed to get id");
+            }
+            Ok(3) => println!("edit a todo"),
+            Ok(4) => {
+                println!("closing application, thanks");
+                process::exit(0);
+            }
+            _ => println!("not a valid option"),
+        }
+    }
+}
+
 fn main() {
     let mut todo_list = Todos::new();
+    show_menu(&mut todo_list);
 
     let new_todo = create_todo();
     todo_list.add(new_todo);
